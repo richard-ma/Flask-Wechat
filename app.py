7eba607804e0be04948a1ae258bf286f68d50d4c
Richard Ma <richard.ma.19850509@gmail.com>
2016-12-25 21:13:41 +0800

    add signature check

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from wechat_sdk import WechatBasic
+from config import conf
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return "index"
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
+
+    wechat = WechatBasic(conf = conf)
+    if wechat.check_signature(signature, timestamp, nonce):
+        return echostr
+    else:
+        return 'Signature check failed'
 
 @app.route('/hello')
 def hello_world():
