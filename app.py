2c706e5f7dad8966753255feaed1d1ec8585a0c2
Richard Ma <richard.ma.19850509@gmail.com>
2016-12-25 21:31:43 +0800

    use env var instead of config file

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, request
-from wechat_sdk import WechatBasic
-from config import conf
+from wechat_sdk import WechatConf, WechatBasic
 
 app = Flask(__name__)
+
+conf = WechatConf(
+    #token = app.config.from_envvar('WECHAT_TOKEN'),
+    appid = app.config.from_envvar('WECHAT_APPID'),
+    appsecret = app.config.from_envvar('WECHAT_SECRET'),
+    #encrypt_mod = app.config.from_envvar('WECHAT_ENCRYPT_MOD'), # normal/compatible/safe -> 明文/兼容/安全
+    #encoding_aes_key = app.config.from_envvar('WECHAT_ENCODING_AES_KEY'),
+)
+wechat = WechatBasic(conf = conf)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -11,7 +21,6 @@ def index():
     nonce = request.args.get('nonce')
     echostr = request.args.get('echostr')
 
-    wechat = WechatBasic(conf = conf)
     if wechat.check_signature(signature, timestamp, nonce):
         return echostr
     else:
