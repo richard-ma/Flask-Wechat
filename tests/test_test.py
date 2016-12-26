ac33b5d7618700e71854ede483692eeeb480a0a5
Richard Ma <richard.ma.19850509@gmail.com>
2016-12-26 09:15:59 +0800

    add env export bash script

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,2 +1,25 @@
-def test_answer():
-    assert 0 == 0
+# -*- coding: utf-8 -*-
+
+from urllib import urlencode
+import unittest
+import app
+
+class FlaskWechatTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def testCheckSignature(self):
+        rv = self.app.get('/')
+        assert 'Signature check failed' == rv.data
+
+        query = {
+            'signature': '',
+            'timestamp': '',
+            'nonce': '',
+            'echostr': '',
+        }
+        rv = self.app.get('/%s' % (urlencode(query)))
+        assert 'Signature check failed' == rv.data
